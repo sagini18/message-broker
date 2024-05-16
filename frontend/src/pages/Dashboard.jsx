69d6d850f0ac07c13be0341307df68e99c90ad15
sagini18 <saginisaju@gmail.com>
2024-05-16 16:15:07 +0530

    table

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
+import DataTable from '../components/Table'
 
 export default function Dashboard() {
   return (
     <div>
       <NavBar/>
-      
+      <DataTable/>
     </div>
   )
 }
