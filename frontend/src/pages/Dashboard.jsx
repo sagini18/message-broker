77b7286d7e5971c32aa2c14341ff5317c933aa47
sagini18 <saginisaju@gmail.com>
2024-05-16 21:14:13 +0530

    card

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import NavBar from "../components/NavBar";
 import DataTable from "../components/Table";
 import BasicLineChart from "../components/Chart";
-import { Box } from "@mui/material";
+import { Box, Stack } from "@mui/material";
+import GraphCard from "../components/Card";
 
 export default function Dashboard() {
   return (
@@ -12,8 +13,13 @@ export default function Dashboard() {
         <BasicLineChart />
         <BasicLineChart />
       </Box>
-      <Box display={"flex"} paddingInline={2}>
+      <Box display={"flex"} paddingInline={2} justifyContent={"space-between"}>
       <DataTable />
+      <Box display={"flex"} flexDirection={"column"} justifyContent={"space-around"}>
+        <GraphCard count={100} name={"No of producers"} color={"#5E35B1"} />
+        <GraphCard count={800} name={"No of consumers"} color={"#35B175"} />
+        <GraphCard count={14} name={"No of channels"} color={"#1E88E5"} />
+      </Box>
       </Box>
     </div>
   );
