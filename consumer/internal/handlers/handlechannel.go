b1d4229b123b5e5438bf7f8fce3da498c2cf5049
sagini18 <saginisaju@gmail.com>
2024-06-18 17:39:10 +0530

    k8s

--- consumer/internal/handlers/handlechannel.go
+++ consumer/internal/handlers/handlechannel.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"fmt"
 	"net"
+	"os"
 )
 
 func HandleChannel(tcpConsumer net.Conn) error {
@@ -10,9 +11,12 @@ func HandleChannel(tcpConsumer net.Conn) error {
 	fmt.Println("Enter the channel number you want to listen to: ")
 	fmt.Scanln(&input)
 
-	for input == "" {
-		fmt.Println("Please provide a channel name. It can't be left blank.")
-		fmt.Scanln(&input)
+	if input == "" {
+		input = os.Getenv("CHANNEL_NAME")
+		for input == "" {
+			fmt.Println("Please provide a channel name. It can't be left blank.")
+			fmt.Scanln(&input)
+		}
 	}
 
 	if _, err := tcpConsumer.Write([]byte(input)); err != nil {
